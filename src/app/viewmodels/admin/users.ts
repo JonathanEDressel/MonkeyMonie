7b9e47359b83909ada5de78fa894b4cc1558096e
Jonathan Dressel <jonathanedressel@users.noreply.github.com>
2025-12-08 14:20:34 -0800

    ability to filter users and there was a fix for site admin check

--- src/app/viewmodels/admin/users.ts
+++ src/app/viewmodels/admin/users.ts
@@ -3,7 +3,7 @@ import { Component, signal } from '@angular/core';
 import { UserController } from '../../services/controllers/usercontroller';
 import { AuthData } from '../../services/authdata';
 import { UserData } from '../../services/userdata';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { UserModel } from '../../models/usermodel';
 import { AsyncPipe } from '@angular/common';
 import { DatePipe } from '@angular/common';
@@ -26,10 +26,33 @@ export class UsersComponent  {
 
     isAdmin$: Observable<boolean>;
     users$: Observable<UserModel[]>;
-    
+
+    filter$: BehaviorSubject<string> = new BehaviorSubject<string>("");
+    usersFiltered$!: Observable<UserModel[]>;
+
     constructor(private _usrData: UserData) {
         this.isAdmin$ = _usrData.isAdmin$;
         this.users$ = _usrData.userList$;
+        this.createUserFilter();
+    }
+
+    createUserFilter(): void {
+        this.usersFiltered$ = combineLatest([
+            this.users$,
+            this.filter$
+        ]).pipe(
+            map(([users, filter]) =>
+                users.filter(u => {
+                        var fullName = (u.FirstName + ' ' + u.LastName).toLowerCase();
+                        var username = u.Username.toLowerCase();
+                        var email = u.Email.toLowerCase();
+                        var f = filter.toLowerCase();
+
+                        return fullName.includes(f) || username.includes(f) || email.includes(f);
+                    }
+                )
+            )
+        );
     }
 
     clearPasswords(): void {
