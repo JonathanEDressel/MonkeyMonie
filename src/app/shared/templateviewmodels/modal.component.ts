46c8a5f78f1f1566476fbb7530a48f07aacbf469
Jonathan Dressel <jonathanedressel@users.noreply.github.com>
2025-12-09 15:44:42 -0800

    modal popup to confirm account deletion

---
+++ src/app/shared/templateviewmodels/modal.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: '../templateviews/modal.component.html',
+  styleUrls: ['../styles/modal.component.scss']
+})
+
+export class ModalPopup {
+  @Input() title: string = "";
+  @Input() message: string = "";
+  @Input() content!: TemplateRef<any> | null;
+
+  @Output() confirmButton = new EventEmitter<void>();
+  @Output() cancelButton = new EventEmitter<void>();
+
+  confirmAction() {
+    this.confirmButton.emit();
+  }
+
+  cancelAction() {
+    this.cancelButton.emit();
+  }
+}
+
--- src/app/viewmodels/portal/accounts.ts
+++ src/app/viewmodels/portal/accounts.ts
@@ -7,10 +7,12 @@ import { AsyncPipe } from '@angular/common';
 import { CurrencyInput } from 'react-currency-input-field';
 import {CurrencyMaskModule } from 'ng2-currency-mask';
 import { CommonModule } from '@angular/common';
+import { ModalPopup } from '../../shared/templateviewmodels/modal.component';
 
 @Component({
   selector: 'accounts-root',
-  imports: [FormsModule, AsyncPipe, CurrencyMaskModule, CommonModule],
+  standalone: true,
+  imports: [FormsModule, AsyncPipe, CurrencyMaskModule, CommonModule, ModalPopup],
   templateUrl: '../../views/portal/accounts.html',
   styleUrl: '../../styles/portal/accounts.scss'
 })
@@ -24,9 +26,10 @@ export class AccountsComponent {
     acctBalance = signal(0);
     personalAccts$: Observable<PersonalAccountModel[]>;
 
-    constructor(private _acctData: AcctData) {
-        this.personalAccts$ = _acctData.personalAccounts$;
-    }
+    deleteTitle = "Delete Account";
+    deleteMsg = "Are you sure you want to delete this account?";
+    showDeleteModal = false;
+    idToDelete = 0;
 
     clearInputs(): void {
         this.acctBalance.set(0);
@@ -34,12 +37,14 @@ export class AccountsComponent {
         this.acctName.set("");
     }
 
-    ngOnInit(): void {
-        this.activate();
-        this._acctData.getPersonalAccounts();
+    closeDeleteModal() {
+        this.showDeleteModal = false;
     }
 
-    activate(): void {
+    openDeleteModal(id: number, name: string) {
+        this.showDeleteModal = true;
+        this.idToDelete = id;
+        this.deleteMsg = "Are you sure you want to delete \"" + name + "\" account?"
     }
 
     addAccount(): void {
@@ -61,8 +66,21 @@ export class AccountsComponent {
         this.closeModal();
     }
 
-    removeAccount(id: number): void {
+    removeAccount(): void {
         this.closeModal();
-        this._acctData.removePersonalAccount(id);
+        this._acctData.removePersonalAccount(this.idToDelete);
+        this.closeDeleteModal();
+    }
+
+    constructor(private _acctData: AcctData) {
+        this.personalAccts$ = _acctData.personalAccounts$;
+    }
+
+    ngOnInit(): void {
+        this.activate();
+        this._acctData.getPersonalAccounts();
+    }
+    
+    activate(): void {
     }
 }
