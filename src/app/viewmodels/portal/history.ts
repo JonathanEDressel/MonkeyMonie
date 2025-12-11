efb689cf89cfff922251ea2c016fed4724e9af09
Jonathan Dressel <jonathanedressel@gmail.com>
2025-12-10 22:12:07 -0800

    added charts with dummy data for each account

--- src/app/viewmodels/portal/history.ts
+++ src/app/viewmodels/portal/history.ts
@@ -1,23 +1,64 @@
 import { FormsModule } from '@angular/forms';
-import { Component, computed, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, computed, signal } from '@angular/core';
 import { AcctData } from '../../services/acctdata';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { ChartData, ChartOptions, ElementChartOptions } from 'chart.js';
+import { AgChartOptions } from 'ag-charts-community';
+import { BaseChartDirective } from "ng2-charts";
+
 
 interface Record {
-    Date: string;
-    Balance: number;
+    Date: Date;
+    Balance: string;
 }
 
 @Component({
   selector: 'history-root',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, BaseChartDirective],
   templateUrl: '../../views/portal/history.html',
   styleUrl: '../../styles/portal/history.scss'
 })
 
 export class HistoryComponent {
+
     historyRaw = signal<any[]>([]);
+
+    formatCurrency(val: number,dec: number = 0): string {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: dec}).format(val);
+    }
+
+    accountHistoryChartOptions: ChartOptions<'line'> = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    autoSkip: true
+                }
+            },
+            y: {
+                beginAtZero: false
+            }
+        }
+    };
+
+    accountHistoryChartData: ChartData<'line'> = {
+        labels: ['January', 'February', 'March', 'April'],
+        datasets: [
+            {
+                label: 'Balance $',
+                data: [5000, 4500, 6000, 9000],
+                fill: false,
+                tension: 0.3
+            }
+        ],
+    }
 
     mappedRecords = computed(() => {
         const items = this.historyRaw();
@@ -25,19 +66,17 @@ export class HistoryComponent {
         items.forEach(x => {
             const key = `${x.AccountId}-${x.Name}`;
             const tmp: Record = {
-                Date: x.RecordedDate,
-                Balance: x.Balance,
+                Date: new Date(x.RecordedDate),
+                Balance: this.formatCurrency(x.Balance),
             };
-
+            var list: Record[] = [];
             if (trackAccounts.has(key)) {
-                var list = trackAccounts.get(key) ?? [];
-                list.push(tmp);
-                trackAccounts.set(key, list);
+                list = trackAccounts.get(key) ?? [];
             }
-            else
-                trackAccounts.set(key, [tmp]);
+            list.push(tmp);
+            list.sort((a, b) => b.Date.getTime() - a.Date.getTime());
+            trackAccounts.set(key, list);
         });
-        console.log(trackAccounts)
         return trackAccounts;
     });
 
@@ -45,8 +84,11 @@ export class HistoryComponent {
         return key.replace(/^\d+-/, '');
     }
 
-    constructor(private _actData: AcctData) {
+    onToggle() {
+        this.cdr.detectChanges();
+    }
 
+    constructor(private _actData: AcctData, private cdr: ChangeDetectorRef) {
     }
 
     ngOnInit(): void {
