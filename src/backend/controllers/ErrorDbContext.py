b4a11f482639e751b93874f5efb20b48279f87c1
Jonathan Dressel <jonathanedressel@users.noreply.github.com>
2025-12-16 14:50:32 -0800

    base done for getting error log. need to move a few things around though before implementing it

--- src/backend/controllers/ErrorDbContext.py
+++ src/backend/controllers/ErrorDbContext.py
@@ -1,6 +1,21 @@
 import traceback
 from datetime import datetime
 import helper.Helper as DBHelper
+import models.ErrorLogModel as ErrorLog
+
+def get_all_errors(day, month, year):
+    try:
+        sql = f"Select * From ErrorLog Where Day(EventTimeStamp) = %s And Month(EventTimeStamp) = %s And Year(EventTimeStamp) = %s;"
+        params = (day, month, year)
+        errors = DBHelper.run_query(sql, params, fetch=True)
+        res = []
+        for e in errors:
+            tmp = ErrorLog.data_to_model(e)
+            res.append(tmp)
+        return res
+    except Exception as e:
+        log_error(e)
+        return None
 
 def log_error(exc: Exception, username = "n/a"):
     try:
