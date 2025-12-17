4f1a1dc14c078f1031dab13c2c310e2c9dd29c94
Jonathan Dressel <jonathanedressel@gmail.com>
2025-12-16 23:18:32 -0800

    reverted changes and made fix

--- src/backend/controllers/AuthDbContext.py
+++ src/backend/controllers/AuthDbContext.py
@@ -75,19 +75,16 @@ def has_admin():
         res = DBHelper.run_query("SELECT Username FROM UserAcct Where Username = %s or Email = %s", 
                                 ("Admin", "jonathanedressel@gmail.com"), 
                                 True)
-        if res:
-            sql = "Update UserAcct set IsActive = 1 Where Id = 1;"
-            DBHelper.run_query(sql, None, fetch=False)
         if not res:
             adm_uuid = DBHelper.create_uuid()
             sql = """
             INSERT INTO UserAcct
             (Username, UserPassword, UUID, FirstName, LastName, Email, CreatedDate,
-            ConfirmedEmail, TwoFactor, AdminLevel, IsAdmin, IsDemo)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ConfirmedEmail, TwoFactor, AdminLevel, IsAdmin, IsDemo, IsActive)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
             params = ("Admin", h, adm_uuid, "Jonathan", "Dressel", "jonathanedressel@gmail.com", currDte,
-                    1, 0, "Site", 1, 0)
+                    1, 0, "Site", 1, 0, 1)
             DBHelper.run_query(sql, params)
     except Exception as e:
         log_error_to_db(e)
@@ -101,8 +98,8 @@ def user_login(username, password):
         usrexpdte = usr['ExpireDate']
         if (not usr):
             return jsonify({"result": "Invalid login credentials", "status": 400}), 400
-        # if (usr['IsActive'] == 0):
-        #     return jsonify({"result": "Account is no longer active", "status": 400}), 400
+        if (usr['IsActive'] == 0):
+            return jsonify({"result": "Account is no longer active", "status": 400}), 400
         if (usrexpdte and usrexpdte < datetime.now()):
             return jsonify({"result": "Account has expired", "status": 400}), 400
 
@@ -133,9 +130,9 @@ def create_account(fname, lname, username, phonenumber, password):
 
         adm_uuid = DBHelper.create_uuid()
         hashedPassword = DBHelper.encrypt_password(password)
-        sql = f"INSERT INTO UserAcct (Username, Email, FirstName, LastName, UserPassword, UUID, PhoneNumber, CreatedDate) " \
-            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"
-        vars = (username, username, fname, lname, hashedPassword, adm_uuid, phonenumber, datetime.now(timezone.utc))
+        sql = f"INSERT INTO UserAcct (Username, Email, FirstName, LastName, UserPassword, UUID, PhoneNumber, CreatedDate, IsActive) " \
+            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        vars = (username, username, fname, lname, hashedPassword, adm_uuid, phonenumber, datetime.now(timezone.utc), 1)
         res = DBHelper.run_query(sql, vars, fetch=False)
         token = get_user_token(username, adm_uuid)
         if not res or not token:
