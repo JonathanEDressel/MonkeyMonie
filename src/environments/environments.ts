140064f3c12391adb34efe921bb7da648a91ab82
Jonathan Dressel <jonathanedressel@users.noreply.github.com>
2025-12-29 07:21:49 -0800

    misc changes

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 //For local
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5000' //'https://api.monkeymonie.com' 
+  apiUrl: 'https://api.monkeymonie.com' //'http://127.0.0.1:5000' 
 };
